2c422801e8b12e11860395988e431a9cabf69c91
Fiona <fxzhou@ucdavis.edu>
2019-08-20 23:17:07 -0700

    liri bot completed, with appendFile bonus

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # liri-node-app
+
+LIRI-Bot
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+LIRI uses the following commands:
+concert-this
+spotify-this-song
+movie-this
+do-what-it-says
+
+Technologies used:
+Node.js
+Javascript
+
+npm packages:
+spotify - A simple to use API library for the Spotify REST API.
+request - Request is designed to be the simplest way possible to make http calls. It supports HTTPS and follows redirects by default.
+dotenv - Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+
+How to Run LIRI-Bot
+One: node liri concert-this
+
+This will show the next upcoming concert for an artist inputted.
+
+Two: node liri spotify-this-song <song name here>.
+
+This will show the following information about the song in your terminal/bash window:
+
+Artist(s)
+The song's name
+A preview link of the song from Spotify
+The album that the song is from
+If no song is provided then the program will default to "The Sign" by Ace of Base
+
+Three: node liri.js movie-this <movie name here>.
+
+This will output the following information to your terminal/bash window:
+
+Title of the movie.
+Year the movie came out.
+IMDB Rating of the movie.
+Country where the movie was released.
+Language of the movie.
+Plot of the movie.
+Actors in the movie.
+
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody'
+
+Step Four: node liri.js do-what-it-says
+
+This will output the command placed in random.txt file
+
+Author
+Connor Paxton
---
+++ liri.js
@@ -0,0 +1,132 @@
+require("dotenv").config();
+var axios = require("axios");
+var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var text = "";
+
+const choice = process.argv[2];
+const input = process.argv.slice(3).join(" ");
+
+switch (choice) {
+  case "concert-this":
+    concert(input);
+    break;
+  case "spotify-this-song":
+    if (input) {
+      spotifyThis(input);
+    } else {
+      spotifyThis("The Sign Ace of Base");
+    }
+    break;
+  case "movie-this":
+    if (input) {
+      omdb(input);
+    } else {
+      omdb("Mr. Nobody");
+    }
+    break;
+  case "do-what-it-says":
+    thing();
+    break;
+  default:
+    console.log("Try again");
+}
+
+//concert-this
+function concert(artist) {
+  var url =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+
+  axios.get(url).then(function(response) {
+    console.log("___Upcoming Events___");
+    text =
+      "Artist: " +
+      artist +
+      "\nVenue: " +
+      response.data[0].venue.name +
+      "\nLocation: " +
+      response.data[0].venue.city +
+      ", " +
+      response.data[0].venue.country +
+      "\nDate: " +
+      response.data[0].datetime.split("T")[0] +
+      "\n";
+    console.log(text);
+    fileAppend();
+  });
+}
+
+//spotify-this-song
+function spotifyThis(song) {
+  spotify.search({ type: "track", query: song }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    text =
+      "\n___Track Info___" +
+      "\nArtist: " +
+      data.tracks.items[0].artists[0].name +
+      "\nSong: " +
+      data.tracks.items[0].name +
+      "\nLink: " +
+      data.tracks.items[0].external_urls.spotify +
+      "\nAlbum: " +
+      data.tracks.items[0].album.name +
+      "\n";
+    console.log(text);
+    fileAppend();
+  });
+}
+
+//movie-this
+function omdb(movie) {
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+  axios.get(queryUrl).then(function(response) {
+    text =
+      "\n___Movie Info___" +
+      "\nTitle: " +
+      response.data.Title +
+      "\nRelease Year: " +
+      response.data.Year +
+      "\nRating: " +
+      response.data.Rated +
+      "\nRelease Country: " +
+      response.data.Country +
+      "\nLanguage: " +
+      response.data.Language +
+      "\nPlot: " +
+      response.data.Plot +
+      "\nActors: " +
+      response.data.Actors +
+      "\n";
+
+    console.log(text);
+    fileAppend();
+  });
+}
+
+//do-what-it-says
+function thing() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(error);
+    }
+    var random = data.split(",");
+    spotifyThis(random[1]);
+  });
+}
+
+function fileAppend() {
+  fs.appendFile("log.txt", "\n" + text, function(err) {
+    // If an error was experienced we will log it.
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
